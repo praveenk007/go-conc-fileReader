f9fad5f4bbad6c92dc74bc695485ea48327e5db5
pk1992 <praveen.k@gmail.com>
2019-02-20 19:12:40 +0530

    file read logic

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func main() {
+	f, err := os.Open("tags.csv")
+	if err != nil {
+		panic(err)
+	}
+	//f.Seek(6, 0)
+	r := bufio.NewReader(f)
+
+	b, pref, err := r.ReadLine()
+	fmt.Println(string(b))
+	fmt.Println(pref)
+
+	b1, pref1, _ := r.ReadLine()
+	fmt.Println(string(b1))
+	fmt.Println(pref1)
 }
